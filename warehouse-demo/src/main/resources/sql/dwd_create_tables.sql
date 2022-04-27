8f0e9a3c5c7c05bebfab32a1f99a06ea0d362507
mwangli <1325533186@qq.com>
2022-04-27 20:39:22 +0800

    dwd

--- warehouse-demo/src/main/resources/sql/dwd_create_tables.sql
+++ warehouse-demo/src/main/resources/sql/dwd_create_tables.sql
@@ -76,4 +76,34 @@ create table dwd_action_log
     partitioned by (dt string)
     stored as parquet
     location '/hive/warehouse/dwd/dwd_action_log'
+    tblproperties ('parquet.compression' = 'lzo');
+
+drop table if exists dwd_display_log;
+create table dwd_display_log
+(
+    area_code         string comment '地区编码',
+    brand             string comment '手机品牌',
+    channel           string comment '渠道',
+    is_new            string comment '是否首次启动',
+    model             string comment '手机型号',
+    mid_id            string comment '设备id',
+    os                string comment '操作系统',
+    user_id           string comment '用户id',
+    version_code      string comment 'APP版本号',
+    during_time       bigint comment '持续时间',
+    page_item         string comment '目标id',
+    page_item_type    string comment '目标类型',
+    last_page_id      string comment '上页id',
+    page_id           string comment '页面id',
+    source_type      string comment '来源类型',
+    display_type      string comment '曝光类型',
+    display_item      string comment '曝光目标id',
+    display_item_type string comment '曝光目标类型',
+    display_order     string comment '曝光顺序',
+    pos_id            string comment '曝光位置',
+    ts                bigint comment '产生时间'
+)
+    partitioned by (dt string)
+    stored as parquet
+    location '/hive/warehouse/dwd/dwd_display_log'
     tblproperties ('parquet.compression' = 'lzo')
--- warehouse-demo/src/main/resources/sql/dwd_load_data.sql
+++ warehouse-demo/src/main/resources/sql/dwd_load_data.sql
@@ -41,7 +41,13 @@ from ods_logs
 where dt = '2022-04-26'
   and get_json_object(line, '$.page') is not null;
 
--- insert overwrite table dwd_action_log partition (dt = '2022-04-26')
+
+drop function if exists explode_json_array;
+create function explode_json_array
+    as 'online.mwang.hive.udtf.ExplodeJSONArray'
+    using jar 'hdfs://mycluster/hive/warehouse/origin/jars/warehouse-udtf.jar';
+
+insert overwrite table dwd_action_log partition (dt = '2022-04-26')
 select get_json_object(line, '$.common.ar'),
        get_json_object(line, '$.common.ba'),
        get_json_object(line, '$.common.ch'),
@@ -57,20 +63,64 @@ select get_json_object(line, '$.common.ar'),
        get_json_object(line, '$.page.last_page_id'),
        get_json_object(line, '$.page.page_id'),
        get_json_object(line, '$.page.source_type'),
-       get_json_object(line, '$.actions'),
+       get_json_object(action, '$.action_id'),
+       get_json_object(action, '$.item'),
+       get_json_object(action, '$.item_type'),
        get_json_object(line, '$.ts')
-from ods_logs
+from ods_logs lateral view explode_json_array(get_json_object(line, '$.actions')) tmp as action
 where dt = '2022-04-26'
   and get_json_object(line, '$.actions') is not null;
 
 
-drop function if exists explode_json_array;
-create function explode_json_array
-as 'online.mwang.hive.udtf.ExplodeJSONArray'
-    using jar 'hdfs://mycluster/hive/warehouse/origin/jars/warehouse-udtf.jar';
 
-select explode_json_array(cast(get_json_object(line, '$.actions') as string))
-from ods_logs
+insert overwrite table dwd_action_log partition (dt = '2022-04-26')
+select get_json_object(line, '$.common.ar'),
+       get_json_object(line, '$.common.ba'),
+       get_json_object(line, '$.common.ch'),
+       get_json_object(line, '$.common.is_new'),
+       get_json_object(line, '$.common.md'),
+       get_json_object(line, '$.common.mid'),
+       get_json_object(line, '$.common.os'),
+       get_json_object(line, '$.common.uid'),
+       get_json_object(line, '$.common.vc'),
+       get_json_object(line, '$.page.during_time'),
+       get_json_object(line, '$.page.item'),
+       get_json_object(line, '$.page.item_type'),
+       get_json_object(line, '$.page.last_page_id'),
+       get_json_object(line, '$.page.page_id'),
+       get_json_object(line, '$.page.source_type'),
+       get_json_object(action, '$.action_id'),
+       get_json_object(action, '$.item'),
+       get_json_object(action, '$.item_type'),
+       get_json_object(line, '$.ts')
+from ods_logs lateral view explode_json_array(get_json_object(line, '$.actions')) tmp as action
 where dt = '2022-04-26'
   and get_json_object(line, '$.actions') is not null;
+
+
+insert overwrite table dwd_display_log partition (dt = '2022-04-26')
+select get_json_object(line, '$.common.ar'),
+       get_json_object(line, '$.common.ba'),
+       get_json_object(line, '$.common.ch'),
+       get_json_object(line, '$.common.is_new'),
+       get_json_object(line, '$.common.md'),
+       get_json_object(line, '$.common.mid'),
+       get_json_object(line, '$.common.os'),
+       get_json_object(line, '$.common.uid'),
+       get_json_object(line, '$.common.vc'),
+       get_json_object(line, '$.page.during_time'),
+       get_json_object(line, '$.page.item'),
+       get_json_object(line, '$.page.item_type'),
+       get_json_object(line, '$.page.last_page_id'),
+       get_json_object(line, '$.page.page_id'),
+       get_json_object(line, '$.page.source_type'),
+       get_json_object(display, '$.display_type'),
+       get_json_object(display, '$.item'),
+       get_json_object(display, '$.item_type'),
+       get_json_object(display, '$.order'),
+       get_json_object(display, '$.pos_id'),
+       get_json_object(line, '$.ts')
+from ods_logs lateral view explode_json_array(get_json_object(line, '$.displays')) tmp as display
+where dt = '2022-04-26'
+  and get_json_object(line, '$.displays') is not null;
 
