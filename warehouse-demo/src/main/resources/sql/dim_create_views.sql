d42e50eb929e0c772ebb2282072cabb69e28e01c
limingwang <dknhfre1st>
2022-05-19 16:21:59 +0800

    創建試圖2

--- warehouse-demo/src/main/resources/sql/dim_create_views.sql
+++ warehouse-demo/src/main/resources/sql/dim_create_views.sql
@@ -9,7 +9,23 @@ select *
 from dim_user_info_view;
 
 create or replace view dim_sku_info_view as
-select *
+select id,
+       price,
+       sku_name,
+       sku_desc,
+       weight,
+       is_sale,
+       spu_id,
+       spu_name,
+       create_time,
+       category3_id,
+       category3_name,
+       category2_id,
+       category2_name,
+       category1_id,
+       category1_name,
+       tm_id,
+       tm_name
 from dim_sku_info
 where dt = current_date;
 
