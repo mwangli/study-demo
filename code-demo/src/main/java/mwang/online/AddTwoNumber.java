c928a0dd9fce1f8f9c4b51eba84b8e86c596114c
mwangli <1325533186@qq.com>
2022-12-19 14:43:34 +0800

    add code-demo

---
+++ code-demo/src/main/java/mwang/online/AddTwoNumber.java
@@ -0,0 +1,20 @@
+package mwang.online;
+
+import java.util.Arrays;
+
+public class AddTwoNumber {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+        int[] res = test1(nums, target);
+        System.out.println(Arrays.toString(res));
+    }
+
+
+    public static int[] test1(int[] source, int target) {
+        int[] result = new int[2];
+        // TODO
+        return result;
+    }
+}
