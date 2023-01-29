18953a71edc774cefe8c3d12e67a35020084a4de
@limingwang <1325533186@qq.com>
2023-01-29 13:57:06 +0800

    统计星号，下一个排列

--- code-demo/src/main/java/mwang/online/SwapListNode.java
+++ code-demo/src/main/java/mwang/online/SwapListNode.java
@@ -14,11 +14,9 @@ public class SwapListNode {
     }
 
     public static ListNode swapPairs2(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
+        if (head == null || head.next == null) return head;
         ListNode temp = head.next;
-        head.next = swapPairs(temp.next);
+        head.next = swapPairs2(temp.next);
         temp.next = head;
         return temp;
     }
