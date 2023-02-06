cc4f7ea64792d7903497696b1bb4d3a0d467a7f1
@limingwang <1325533186@qq.com>
2023-02-06 14:44:50 +0800

    组合总数-递归回溯

---
+++ code-demo/src/main/java/mwang/online/hot100/No39_CombinationSum.java
@@ -0,0 +1,40 @@
+package mwang.online.hot100;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 39. 组合总和
+ * 思路：递归回溯剪枝
+ */
+public class No39_CombinationSum {
+
+    public static void main(String[] args) {
+        int[] ints = {2, 3, 6, 7};
+        System.out.println(combinationSum(ints, 7));
+    }
+
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        ArrayList<List<Integer>> ans = new ArrayList<>();
+        ArrayList<Integer> res = new ArrayList<>();
+        dfs(candidates, target, 0, 0, ans, res);
+        return ans;
+    }
+
+    public static void dfs(int[] candidates, int target, int sum, int index, List<List<Integer>> ans, List<Integer> path) {
+        if (sum >= target) {
+            if (sum == target) ans.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = index; i < candidates.length; i++) {
+            int candidate = candidates[i];
+            sum += candidate;
+            path.add(candidate);
+            // 可选范围缩小确保不重复
+            dfs(candidates, target, sum, i, ans, path);
+            // 撤销
+            path.remove(path.lastIndexOf(candidate));
+            sum -= candidate;
+        }
+    }
+}
