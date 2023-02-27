6bdba79063a96f9cbc142eeb2762b8256038ce0e
@limingwang <1325533186@qq.com>
2023-02-27 14:09:16 +0800

    二叉搜索数验证

---
+++ code-demo/src/main/java/mwang/online/hot100/No98_TreeSortCheck.java
@@ -0,0 +1,40 @@
+package mwang.online.hot100;
+
+import mwang.online.base.TreeNode;
+
+import java.util.Stack;
+
+/**
+ * 98. 验证二叉搜索树
+ * Binary Sort Tree
+ * 思路：递归遍历有序
+ */
+public class No98_TreeSortCheck {
+
+
+    private static boolean flag = true;
+
+    public static void main(String[] args) {
+//        final TreeNode root = new TreeNode(5, new TreeNode(1), new TreeNode(4, new TreeNode(3), new TreeNode(6)));
+        final TreeNode root = new TreeNode(0, new TreeNode(0), new TreeNode(0));
+        System.out.println(isValidBST(root));
+    }
+
+    public static boolean isValidBST(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        long preValue = Long.MIN_VALUE;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (root.val <= preValue) {
+                return false;
+            }
+            preValue = root.val;
+            root = root.right;
+        }
+        return true;
+    }
+}
