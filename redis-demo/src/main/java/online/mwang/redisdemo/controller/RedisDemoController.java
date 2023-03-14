36b272ebc2a9b4b7437d407dd4f4b7928f81b8ad
@limingwang <1325533186@qq.com>
2023-03-14 13:52:47 +0800

    redis-demo init

---
+++ redis-demo/src/main/java/online/mwang/redisdemo/controller/RedisDemoController.java
@@ -0,0 +1,24 @@
+package online.mwang.redisdemo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @version 1.0.0
+ * @author: mwangli
+ * @date: 2023/3/14 13:42
+ * @description: RedisDemoController
+ */
+@RestController
+public class RedisDemoController {
+
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
+    @RequestMapping("/test")
+    public String test() {
+        return "v1.0 : 当前访问人数" + redisTemplate.opsForValue().increment("count");
+    }
+}
