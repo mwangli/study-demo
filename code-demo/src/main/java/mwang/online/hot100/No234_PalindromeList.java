5021923fe357fb8f517e0b291953e4c7d7a01b46
@limingwang <1325533186@qq.com>
2023-03-23 10:58:35 +0800

    回文链表-快慢指针二分，栈回文

--- code-demo/src/main/java/mwang/online/hot100/No234_PalindromeList.java
+++ code-demo/src/main/java/mwang/online/hot100/No234_PalindromeList.java
@@ -25,6 +25,7 @@ public class No234_PalindromeList {
             stack.push(p1);
             p1 = p1.next;
             p2 = p2.next;
+
             if (p2 == null) {
                 flag = true;
             } else {
