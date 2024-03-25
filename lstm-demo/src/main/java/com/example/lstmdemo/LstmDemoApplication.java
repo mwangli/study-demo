040547506ad580ee88f79d4e46cc9ed43619adde
mwangli <1325533186@qq.com>
2024-03-25 19:48:03 +0800

    init

---
+++ lstm-demo/src/main/java/com/example/lstmdemo/LstmDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.lstmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LstmDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LstmDemoApplication.class, args);
+    }
+
+}
